dcca5341cba7b7079e984247ebe2bd2c495d84cd
Ubuntu <ubuntu@ip-172-31-23-213.ec2.internal>
2025-12-25 03:39:43 +0000

    Intentionally break test to validate CI quality gate

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../src/app");
 
 test("GET / should return success message", async () => {
-  const res = await request(app).get("/");
-  expect(res.text).toBe("GitHub Actions CI Pipeline Working!");
+	  const res = await request(app).get("/")
+	expect(res.text).toBe("This text will fail");
 });
 

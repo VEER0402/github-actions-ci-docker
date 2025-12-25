8237b2a0d8fb833ba73b4e600078164bb78f1d1f
Ubuntu <ubuntu@ip-172-31-23-213.ec2.internal>
2025-12-25 03:23:21 +0000

    Add Node.js app and tests for CI pipeline

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+test("GET / should return success message", async () => {
+  const res = await request(app).get("/");
+  expect(res.text).toBe("GitHub Actions CI Pipeline Working!");
+});
+
